4c3245d9fa129eacc19c6fcb3b3bcd4ff853dd80
UmangMehar <umangmehar6@gmail.com>
2025-11-15 21:48:02 +0530

    initial commit: add base application setup and core modules

---
+++ src/test/java/in/umangcraft/moneymanager/MoneymanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package in.umangcraft.moneymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneymanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
